596fc2107a751aea7d4eb581671ad40b619458bb
Caren Garcia <carenvaleria90@gmail.com>
2015-05-03 21:51:33 -0500

    Committing hw w01d04 correctly.

---
+++ assignments/w01/d04/adventurew01d04.js
@@ -0,0 +1,72 @@
+// It's dangerous to go alone, take this code with you! 
+
+var name = prompt("What's your name?"){
+	console.log("Welcome to Hyrule" +" "+ name);
+}
+
+var number = prompt("Choose a number 1-10");
+	if (number <= 3) {
+		console.log("You walk into a dark dungeon and alas you find yourself with Ganandorf!");
+			var fight = prompt("Do you use a deku nut and run or use the master sword and fight?"){
+				if (fight == "run") {
+					console.log("Your scrawny little legs couldn't outrun him, you have been annihilated!");
+				}
+				else{
+					console.log("You have fought well and have defeated the evil Ganandorf, well done!");
+				}
+		
+	else if (number > 7 ) {
+		console.log("You climb up to Death Mountain and find the giant fairy randomly floating above a pool of water.");
+			var flirt = prompt("Do you decide to flirt with the great fairy, dash out or stand still and look around aimlessly?"){
+				if(flirt == "flirt"){
+					console.log("You're an odd one alright, great fairy strips you of all your magic powers.");
+				}
+				else if(flirt == "run" || "dash"){
+					console.log("You run out and a big chunck of rock falls upon you and brings you to your untimely death.");
+				}
+				else(flirt == "look"){
+					console.log("Do something productive with your life. Perhaps talking to the Great Fairy. No? Fine, enjoy your listlessness.")
+				}
+			}
+		else() {
+		console.log("You find yourself swimming with Zoras. Awesome!");
+			var reply = prompt("Princess Ruto asks if you could carry her sorry butt around town, do you accept? Yes or No?"){
+				if(reply == "yes"){
+					console.log("Great, now you're carrying around a big fish with you.");
+				}
+				else(reply == "no"){
+					console.log("She is hurt, and retreats to the belly of a big whale.")
+				}
+			};
+	
+var help = prompt("Oh snap, your triforce senses are tingling! This can only mean one thing, Zelda's in danger! Will you help the princess? Y/N");
+	if (help == "y" || "Y") {
+		console.log("Great, you dash into Hyrule castle and a band of skultulas block your entrance, what's your weapon of choice? (A)rrows, (B)ombs or (S)word?");
+				if (help == "A" || "a") {
+					console.log("Hurray, your arrows made mince meat of those blasted things!");
+				}
+				else if(help == "B" || "b"{
+					console.log("Boom, skultulas were obliterated!");
+				}else(){
+					console.log("Hyaaahhh! You sliced and diced those suckers to not!")
+				}
+		
+
+		else(help == "n" || "N") {
+		console.log("Well, I guess Zelda can fend for herself.");
+
+
+var survey = prompt("Did you enjoy your short yet memorable adventure? Y/N?");
+	if (survey == "y" || "Y") {
+		console.log("Glad to hear, I'll try expanding on it again in the future!");
+	};
+	else(survey == "n" || "N"){
+		console.log("It is ok, I don't have feelings.")
+	}
+	
+		
+
+
+
+
+
